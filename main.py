d3b9e23cf9d9ae3e6d1af5e6f2f7a658e70c8388
pipitiaotiao <821946940@qq.com>
2021-01-15 10:51:02 +0800

    excel数据

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import xlrd
+
+
 def excel():
     data = xlrd.open_workbook('导出关系数据0102 的副本.xls')
     table = data.sheets()[0]
@@ -13,30 +15,36 @@ def excel():
         user_id.append(name2)
         friends_id.append(user_id)
     return friends_id
-a = excel()
-#print(a)
 
-list=[]
+
+a = excel()
+# print(a)
+
+list = []
+
+
 def countchar(var):
     result = {}
-    for key in a:   #第一个元素 ['100000163225268', '100003812812018']
+    for key in a:  # 第一个元素 ['100000163225268', '100003812812018']
         list.append(key[1])
     for i in var:
         if i in result:
-            result[i] = result[i]+1
+            result[i] = result[i] + 1
         else:
             result[i] = 1
     return result
-s=countchar(list)
-#print(s)
 
-b = sorted(s.items(), key=lambda x: x[1], reverse=True)#正序排序
-#print(b)
-res = b[0:10:1]   #前十排名
-#print(res)
 
-c = [x[0] for x in res]#只取排名前十的id
-#print(c)
+s = countchar(list)
+# print(s)
+
+b = sorted(s.items(), key=lambda x: x[1], reverse=True)  # 正序排序
+# print(b)
+res = b[0:10:1]  # 前十排名
+# print(res)
+
+c = [x[0] for x in res]  # 只取排名前十的id
+# print(c)
 
 i = 0
 x = 0
@@ -44,25 +52,32 @@ y = []
 for i in c:
     for x in a:
         if i == x[1]:
-            #print(x[0])
+            # print(x[0])
             y.append(x[0])
-#print(y)
+# print(y)
+
 # result2 = str(res)
 # with open(r'C:\Users\16317\PycharmProjects\pythonProject3\111.txt', mode='w') as f:
 #  for i in res:
 #    f.write(result2)
 
-
 import xlwt
-def data_write(y):
-    file = xlwt.workbook(encoding='utf-8')
-    sheet = file.add_sheet(u'final', cell_overwrite_ok=True)
-    i = 0
-    for data in y:
-        for j in range(len(data)):
-            sheet.write(i, j, data[j])
-        i = i + 1
-    file.save('结果.xlsx')
+file = xlwt.Workbook(encoding='utf-8')
+table = file.add_sheet('结果')
 
-#输出到txt文本，在excel把十共同好友列表输入excel
-#mysql 的 安装和链接   增删改查。。
+for i, p in enumerate(y):
+        table.write(i, 0, p)
+file.save('结果.xls')
+# import xlwt
+# from xlwt import *
+#
+# def data_write(y):
+#     file = xlwt.Workbook(encoding='utf-8')
+#     sheet = file.add_sheet(u'final', cell_overwrite_ok=True)
+#     for data in y:
+#         for j in range(len(data)):
+#             sheet.write(j, 1, data[j])
+#     file.save('结果.xls')
+
+# 输出到txt文本，在excel把十共同好友列表输入excel
+# mysql 的 安装和链接  增删改查。。
